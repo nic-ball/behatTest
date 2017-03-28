6f7914da5e1db776c45ed6d8c5e323fa4dafd559
nunchuck <nicholas.ball@sainsburys.co.uk>
2017-03-28 16:25:46 +0100

    Added all files used for the behat test

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,58 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    private $shelf;
+    private $basket;
+
+    public function __construct()
+    {
+        $this->shelf = new Shelf();
+        $this->basket = new Basket($this->shelf);
+    }
+
+    /**
+     * @Given there is a :product, which costs £:price
+     */
+    public function thereIsAWhichCostsPs($product, $price)
+    {
+        $this->shelf->setProductPrice($product, floatval($price));
+    }
+
+    /**
+     * @When I add the :product to the basket
+     */
+    public function iAddTheToTheBasket($product)
+    {
+        $this->basket->addProduct($product);
+    }
+
+    /**
+     * @Then I should have :count product(s) in the basket
+     */
+    public function iShouldHaveProductInTheBasket($count)
+    {
+        Assert::assertCount(intval($count), $this->basket);
+    }
+
+    /**
+     * @Then the overall basket price should be £:price
+     */
+    public function theOverallBasketPriceShouldBePs($price)
+    {
+        Assert::assertSame(floatval($price), $this->basket->getTotalPrice());
+    }
+}
+
+
+
